bed7319c1e468fbb99ddab6a67cbaa074fbdbc47
Bruno Costa <brunocf.dev@hotmail.com>
2023-02-09 21:49:31 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using ExemploDeClasses;
+
+Carro carroFerrari = new Carro();
+carroFerrari.cor = "Vermelha";
+carroFerrari.modelo = "F75";
+carroFerrari.marca = "Ferrari";
+
+Carro carroMercedes = new Carro();
+carroMercedes.cor = carroFerrari.cor;
